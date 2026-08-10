ab8fbedd714512e3a9ab51b74292e995fd1197b4
SzSouza <95456183+Danzhublok@users.noreply.github.com>
2026-08-10 00:30:54 -0300

    Refactor setDados to use functional update

--- src/lib/store.tsx
+++ src/lib/store.tsx
@@ -179,7 +179,8 @@ export function StoreProvider({
         (representation) =>
           representation.id === representationId || representation.name === representationName,
       );
-      setDados({
+      setDados((atual) => ({
+        vendas: atual.vendas ?? [],
         metaMensal: Number(companyTargets?.[0]?.amount ?? 0),
         metasEquipe,
         metasColaborador,
@@ -209,7 +210,7 @@ export function StoreProvider({
               };
             }),
         })),
-      });
+      }));
       setPronto(true);
     };
     void load();
